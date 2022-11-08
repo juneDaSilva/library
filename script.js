e74fde20684669ca95dcca9565261e55686ac4f4
June <adasilva.html@gmail.com>
2022-11-08 09:37:40 -0500

    Make basic javascript functions, index/css files
    Add myLibrary array.
    Make a constructor for the books.
    Add a book log function.
    Run a couple of tests on console.

---
+++ script.js
@@ -0,0 +1,40 @@
+// ------ MAIN WORKING PIECES -----
+
+// Library array
+let myLibrary = [];
+
+// Book constructor
+function Book(title, author, pages, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readStatus = Boolean(readStatus);
+}
+
+// Log book function
+function addBook(book) {
+  myLibrary.push(book);
+}
+
+// ------- ------    ---    ------ -------
+
+
+console.log(myLibrary);
+
+// const omats = new Book('The Old Man and the Sea', 'Ernest Hemingway', 150)
+
+addBook(new Book( 'The Old Man and the Sea', 
+                  'Ernest Hemingway', 
+                  150));
+
+console.log(myLibrary);
+
+// const mf = new Book('A Moveable Feast', 'Ernest Hemingway', 300, true)
+
+addBook(new Book( 'A Moveable Feast', 
+                  'Ernest Hemingway', 
+                  300, 
+                  true));
+
+console.log(myLibrary);
+console.log(myLibrary[1].readStatus);
