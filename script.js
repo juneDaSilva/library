c868ef6b6c94c742f1b41c38130f6f01c7724029
June <adasilva.html@gmail.com>
2022-11-12 15:50:06 -0500

    Add remove book button

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function Book(title, author, pages, readStatus) {
   this.readStatus = Boolean(readStatus);
 }
 
+
 // ------- -------- Table populator 
 
 function buildTable(library) {
@@ -38,8 +39,38 @@ function buildTable(library) {
 
         row.append(cell);
       }
-    tbody.append(row);
+      // create one last cell for a remove button 
+      let removeCell = document.createElement('td');
+      removeCell.classList.add('bodyCell');
+
+      // create inner image element to put inside remove cell
+      let removeImg = document.createElement('img');
+      removeImg.classList.add('removeButton'); // add class that will allow removeImg to inherit properties from css
+      removeImg.value = library[i].title;
+      
+      removeCell.append(removeImg); // add removeImg into new removeCell
+
+      row.append(removeCell); // add new removeCell into row
+      
+      tbody.append(row); // add new row into table body
   };
+
+  // Adding the removeButton listener inside the build table function somehow works
+  // whereas building it outside doesnt. Dont know why.
+  removeButton = document.querySelectorAll('.removeButton');
+
+  removeButton.forEach((button) => {
+    button.addEventListener('click', () => {
+      // console.log(button.value);
+      
+      for (let i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].title === button.value) {
+          myLibrary.splice(i, 1);
+          buildTable(library);
+        }
+      }
+    });
+  });
 }
 
 // ------- ------ Book log form ------ -------
@@ -92,12 +123,37 @@ form.addEventListener("submit", (e) => {
   author.value = '';
   pages.value = '';
   read.checked = false;
-  
+
   e.preventDefault();
 })
 
-
-
 // ------- ------- run functions ------- ------
+
+var book1 = new Book('The Sun Also Rises', 'Ernest Hemingway', 147, true);
+var book2 = new Book('Women', 'Charles Bukowski', 291, true);
+var book3 = new Book('Notes From The Underground', 'Fyodor Dosteovsky', 124, true);
+
+addBook(book1);
+addBook(book2);
+addBook(book3);
+
 buildTable(myLibrary);
+
+
+// ------- -------- Remove book button -------- --------
+
+// removeButton = document.querySelectorAll('.removeButton');
+
+// removeButton.forEach((button) => {
+//   button.addEventListener('click', () => {
+//     // console.log(button.value);
+    
+//     for (let i = 0; i < myLibrary.length; i++) {
+//       if (myLibrary[i].title === button.value) {
+//         myLibrary.splice(i, 1);
+//         console.log(myLibrary);
+//       }
+//     }
+//   });
+// });
 
